76a1f029dcf38cfa1a424211a2f3e9373c0f07c1
Poesgamer <Moosmegens@gmail.com>
2024-08-06 21:33:18 +0200

    as far as i can tell minmax works

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <limits.h>
 #include "bitBoard.h"
 #include "move.h"
 #include "checkWin.h"
@@ -11,19 +12,77 @@ int min(int a, int b) {
     return (a < b) ? a : b;
 }
 
+int minimax(Bitboard *board, int depth, int isMaximizingPlayer, int alpha, int beta){
+    printf("starting minimax\n");
+
+    int player1Win = checkWin(&board->player1);
+    int player2Win = checkWin(&board->player2);
+
+    if (depth == 0) {
+        return 0;
+    }
+
+    int bestValue = (isMaximizingPlayer == 1)? INT_MIN : INT_MAX;
+    if (isMaximizingPlayer) {
+        for (int col = 0; col < WIDTH; col++) {
+            if (openColumn(board, col)) {
+                playMove(board, &board->player1, col);
+                int alreadyWon = checkWin(&board->player1);
+                undoMove(&board->player1, col);
+                alpha = max(alpha, bestValue);
+                if (alreadyWon) {
+                    return 1;
+                }
+            }
+        }
+        for (int col = 0; col < WIDTH; col++) {
+            if (openColumn(board, col)) {
+                playMove(board, &board->player1, col);
+                bestValue = max(bestValue, minimax(board, depth - 1, 0, alpha, beta));
+                undoMove(&board->player1, col);
+                alpha = max(alpha, bestValue);
+                if (beta <= alpha) break;
+            }
+        }
+    } else {
+        for (int col = 0; col < WIDTH; col++) {
+            if (openColumn(board, col)) {
+                playMove(board, &board->player2, col);
+                int alreadyWon = checkWin(&board->player2);
+                undoMove(&board->player2, col);
+                beta = min(beta, bestValue);
+                if (alreadyWon) {
+                    return -1;
+                }
+            }
+        }
+        for (int col = 0; col < WIDTH; col++) {
+            if (openColumn(board, col)) {
+                playMove(board, &board->player2, col);
+                bestValue = min(bestValue, minimax(board, depth - 1, 1, alpha, beta));
+                undoMove(&board->player2, col);
+                beta = min(beta, bestValue);
+                if (beta <= alpha) break;
+            }
+        }
+    }
+
+    return bestValue;
+}
+
 int main (int argc, char *argv[])
 {
     precomputeWinMasks(); 
     Bitboard game;
     boardStart(&game);
-    playMove(&game, &game.player1, 6);
-    playMove(&game, &game.player1, 5);
-    playMove(&game, &game.player1, 4);
-    playMove(&game, &game.player1, 3);
+    playMove(&game, &game.player2, 3);
+    playMove(&game, &game.player2, 2);
+    playMove(&game, &game.player2, 4);
+    
 
 
 
-    printf("Check win: %d\n", checkWin(&game.player1));
+    printf("MiniMax: %d\n", minimax(&game, 3, 1, INT_MIN, INT_MAX));
     printBoard(&game);
 
 

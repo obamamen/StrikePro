5cda62c31e90ef632da8d0ef35ab62e83e9783b3
Poesgamer <Moosmegens@gmail.com>
2024-08-08 22:34:05 +0200

    un zobrist bs

--- main.c
+++ main.c
@@ -1,84 +1,8 @@
 #include <stdio.h> 
-#include <stdlib.h>
-#include <time.h>
 #include <limits.h>
 #include "bitBoard.h"
 #include "move.h"
 #include "checkWin.h"
-
-#define DEPTH 3
-#define HASHSIZE 10007
-
-typedef struct {
-    ULL hash;
-    int score;
-} TableEntry;
-
-ULL zobristHash[WIDTH][HEIGHT][2];
-TableEntry **HashMap;
-
-int getHashSize(int depth) {
-    return (depth)*HASHSIZE;
-}
-
-TableEntry** initializeHashTable() {
-    TableEntry** hashTable = malloc(sizeof(TableEntry*)*DEPTH);
-    if (hashTable == NULL) {
-        printf("Couldn't allocate hastable\n");
-        return NULL;
-    }
-    for (int i = 0; i < DEPTH; i++) {
-        hashTable[i] = malloc(sizeof(TableEntry)*getHashSize(i));
-        if (hashTable[i] == NULL) {
-            printf("Error: Failed to allocate memory for hash table.\n");
-            for (int j = 0; j < i; j++) {
-                free(hashTable[j]);
-            }
-            free(hashTable);
-            return NULL;
-        }
-        for (int j = 0; j < getHashSize(i); j++) {
-            hashTable[i][j].hash = 0ULL;
-            hashTable[i][j].score = 0;
-        }
-    }
-
-    return hashTable;
-}
-
-int hashTableIndex(ULL hash, int depth) {
-    return hash % getHashSize(depth);
-}
-
-void initializeZobristHashTable() {
-    srand(time(NULL));
-    for (int x = 0; x < WIDTH; x++) {
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int i = 0; i < 2; i++) {
-                zobristHash[x][y][1] = ((unsigned long long)rand() << 32) | rand();
-            }
-        }
-    }
-}
-
-ULL getHash(Bitboard *bitboard) {
-    ULL hash = 0;
-    for (int x = 0; x < WIDTH; x++) {
-        for (int y = 0; y < HEIGHT; y++) {
-            if (bitboard->player1 & (1ULL << (x + y * WIDTH))) {
-                hash ^= zobristHash[x][y][0];
-            }
-        }
-    }
-    for (int x = 0; x < WIDTH; x++) {
-        for (int y = 0; y < HEIGHT; y++) {
-            if (bitboard->player2 & (1ULL << (x + y * WIDTH))) {
-                hash ^= zobristHash[x][y][1];
-            }
-        }
-    }
-    return hash;
-}
 
 int count = 0;
 
@@ -92,27 +16,6 @@ int min(int a, int b) {
 
 int minimax(Bitboard *board, int depth, int isMaximizingPlayer, int alpha, int beta){
     //printf("starting minimax\n");
-
-    int aDepth = DEPTH - depth;
-    ULL hash = getHash(board);
-    if (aDepth > 0 && aDepth < DEPTH) {
-        int size = getHashSize(aDepth);
-        int index = hashTableIndex(hash, aDepth);
-
-        if (index < size) {  
-            TableEntry *entry = &HashMap[aDepth][index];
-
-            if (entry->hash == hash) {
-                printf("Hash match found: score = %d\n", entry->score);
-                return entry->score;
-            } else {
-                printf("Hash mismatch: stored hash = %llu, current hash = %llu\n", entry->hash, hash);
-            }
-        } else {
-            printf("Index %d is out of bounds for size %d\n", index, size);
-        }
-    }
-
     count ++;
     int player1Win = checkWin(&board->player1);
     int player2Win = checkWin(&board->player2);
@@ -173,15 +76,6 @@ int minimax(Bitboard *board, int depth, int isMaximizingPlayer, int alpha, int b
         }
     }
 
-    if (aDepth > 0 && aDepth < DEPTH) {
-        int index = hashTableIndex(hash, aDepth);
-        if (index < getHashSize(aDepth)) {
-            TableEntry *entry = &HashMap[aDepth][index];
-            entry->hash = hash;
-            entry->score = bestValue;
-        }
-    }
-
     return bestValue;
 }
 
@@ -207,8 +101,6 @@ int getBestMove(Bitboard *board, int depth, int isMaximizingPlayer) {
 
 int main (int argc, char *argv[])
 {
-    HashMap = initializeHashTable();
-    initializeZobristHashTable();
     precomputeWinMasks(); 
     Bitboard game;
     boardStart(&game);
@@ -220,7 +112,7 @@ int main (int argc, char *argv[])
 
 
     while (1) {
-        playMove(&game, &game.player1, getBestMove(&game, DEPTH, 1));
+        playMove(&game, &game.player1, getBestMove(&game, 14, 1));
 
         //int player1Win = checkWin(&game->player1);
         //int player2Win = checkWin(&game->player2);
